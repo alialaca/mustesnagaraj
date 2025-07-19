61ec97afdc48b04b6f624906356a2c90c9b2350d
alialaca <alialaca0@gmail.com>
2025-07-19 14:47:35 +0300

    Add Umami analytics script to `nuxt.config.ts` for website tracking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,13 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+      ],
+      script: [
+        {
+          src: 'https://cloud.umami.is/script.js',
+          'data-website-id': '5f549c4f-1ebc-4c6c-b191-b2c3e20e2fb4',
+          async: true,
+        }
       ]
     }
   }
