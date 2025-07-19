00acb09bfc2c3348e46ed5cf4d140ee49a066313
alialaca <alialaca0@gmail.com>
2025-07-19 22:55:14 +0300

    Track route changes using Umami analytics in `app.vue`.

--- app.vue
+++ app.vue
@@ -6,3 +6,22 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  watch(
+      () => router.currentRoute.value.fullPath,
+      () => {
+        if (window.umami) {
+          window.umami.track()
+        }
+      },
+      { immediate: true }
+  )
+})
+</script>
